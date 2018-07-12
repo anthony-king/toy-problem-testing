78be8a4416fd3c2eb68a15427a597420ea67cb84
JohnWebb4 <John.Webb94@gmail.com>
2018-07-11 19:23:16 -0700

    Add sum three to new tests

--- new.test.js
+++ new.test.js
@@ -1,1 +1,5 @@
-import './3Sum/3Sum.test';
+// Any new tests are put here.
+// This allows me to only test
+// The new problems, and not old stuff
+// That is already passing.
+import './3sum/3sum.test';
